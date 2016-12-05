aedfdf1dd352fb4aac9df25cf4d8eb04a19ffc40
windski <1272162048@qq.com>
2016-12-05 18:38:10 +0800

    upgrade

--- book.c
+++ book.c
@@ -1,7 +1,33 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 #include"book.h"
 
 booktype *creatdata(void){
+    unsigned long x = 1;
+    booktype *head = (booktype *)malloc(sizeof(booktype));
+    head->next = NULL;
+    while(x != 0){
+        scanf("请输入书的序列号:%lu" &x); 
+        booktype *s = (booktype *)malloc(sizeof(booktype));
+        s->index = x;
+        printf("请输入书的名字\n");
+        fgets(s->name, MAXNAME, stdin);
+        printf("请输入书的作者\n");
+        fgets(s->author, MAXNAME, stdin);
+        s->next = head->next;
+        head->next = s;
+    }
+    return head;
+}
 
+int searchbook(booktype *head, char *bookname){
+    booktype *h = head->next;
+    int storge = 0;
+    while(h->next == NULL){
+        if(strcmp(h->name, bookname) == 0){
+            storge++;
+
+        }
+    }
 }
--- book.h
+++ book.h
@@ -11,13 +11,12 @@ typedef struct booktype{
     char name[MAXNAME];    //图书的书名
     char author[MAXNAME];    //图书的作者
     int flag = 1;     //图书被借出时,flag = 0
-    long num = 1;
 }booktype;
 
 booktype *creatdata(void);    //初始化时录入图书数据
-booktype *addnewbook(booktype *);    //新增图书
-int searchbook(booktype *);    //检索图书
-int loadbook(booktype *);    //借出图书
-int repay(booktype *);    //还书
+int addnewbook(booktype *);    //新增图书
+int searchbook(booktype *, char *);    //检索图书
+int loadbook(booktype *, char *);    //借出图书
+int repay(booktype *, char *);    //还书
 
 #endif
